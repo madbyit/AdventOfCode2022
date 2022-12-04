1ee7f5d816a8130a0d945f86ce053725c8b65b56
Madilyn Bystedt <madilyn@bystedts.com>
2022-12-04 21:40:06 +0100

    wip: First draft sum calories

---
+++ AOC_1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace adventofcode
+{
+    class Program
+    {
+        //List<int> calories_list = new List<int>();
+        public List<int> elf_nr_one = new List<int> {1000, 2000, 3000};
+        public List<int> elf_nr_two = new List<int> {4000};
+        public  List<int> elf_nr_three = new List<int> {5000, 6000};
+        public List<int> elf_nr_four = new List<int> {7000, 8000, 9000};
+        public List<int> elf_nr_five = new List<int> {10000};
+
+        private int CaloriesSum(List<int> elf_calories)
+        {
+            int sum = 0;
+            foreach(var cal in elf_calories)
+                sum += cal;
+            return sum;
+        }
+
+        static void Main ()
+        {
+            Program p = new Program();
+            int sum = p.CaloriesSum(p.elf_nr_two);
+            Console.WriteLine("ElfOne: " + sum);
+        }
+
+    }
+}
