ae96cfd5a3efcea210c0fa34480adfd29e8cf9ac
Madilyn Bystedt <madilyn@bystedts.com>
2022-12-06 20:47:45 +0100

    wip: Draft AOC day 3 part 1

---
+++ AOC_3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace adventofcode
+{
+    /**
+    * Advent of code Day 3
+    */
+    class Program
+    {
+        private void Init()
+        {
+           Console.WriteLine("*** INIT ***");
+        }
+            
+        // Part 1
+        private void TBD()
+        {
+            Init();
+            
+            foreach (string line in File.ReadLines("puzzleinput.txt"))
+            {
+                 Console.WriteLine("Line:" + line);
+            }
+        }
+
+
+        static void Main ()
+        {
+            Program p = new Program();
+            p.TBD();
+        }
+    }
+}
