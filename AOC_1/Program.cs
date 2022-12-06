ce6eeef518c6576694c8749a502ddb86df5d7339
Madilyn Bystedt <madilyn@bystedts.com>
2022-12-06 08:12:19 +0100

    Add solution for AOC1 part 2

--- AOC_1/Program.cs
+++ AOC_1/Program.cs
@@ -1,14 +1,67 @@
 ﻿namespace adventofcode
 {
+    /**
+    * Advent of code Day 1
+    */
     class Program
     {
-        private void ElfCarriedCaloriesCounter()
+
+        private int countElfs, sum, totalTopThree, max, max1, max2, max3, elfwithmaxcalories;
+    
+        private void init()
         {
+            countElfs = 0;
+            sum = 0;
+            totalTopThree = 0;
+            max1 = 0;
+            max1 = 0;
+            max2 = 0;
+            max3 = 0;
+            elfwithmaxcalories = 0;
+        }
+
+        // Part 2
+        private void TopThreeCaloriesCounter()
+        {
+            init();
+            // Read the file and display it line by line.  
+            foreach (string line in File.ReadLines("inputlist.txt"))
+            {  
+                if(line != string.Empty)
+                {
+                    sum += Int32.Parse(line);
+                }
+                else
+                {
+                    
+                    if(sum > max3 && sum > max2 && sum > max1)
+                    {
+                        max3 = sum;
+                    }
+                    if(sum < max3 && sum > max2 && sum > max1 )
+                    {
+                        max2 = sum;
+                    }
+                    if(sum > max1 && sum < max2 && sum < max3)
+                    {
+                        max1 = sum;
+                    }
+
+                    sum = 0;
+                }
+            } 
+            totalTopThree = max1 + max2 + max3; 
+            
+            Console.WriteLine("Total top 3 calories: {0}", totalTopThree);  
+
+        }
+
+        // Part 1
+        private void MaxCaloriesCounter()
+        {
+            init();
             int countElfs = 1;
-            int sum = 0;
-            int max = 0;
-            int elfwithmaxcalories = 0;
-  
+    
             // Read the file and display it line by line.  
             foreach (string line in File.ReadLines("inputlist.txt"))
             {  
@@ -35,13 +88,8 @@
         static void Main ()
         {
             Program p = new Program();
-            p.ElfCarriedCaloriesCounter();
-            
-            // Suspend the screen.
-            Console.WriteLine("Press any key."); 
-            Console.ReadLine();  
-        
+            p.MaxCaloriesCounter();
+            p.TopThreeCaloriesCounter();
         }
-
     }
 }
