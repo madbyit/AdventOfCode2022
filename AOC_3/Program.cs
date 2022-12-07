110f0baec2fbd07f8692a67e08dad597a9f6b48b
Madilyn Bystedt <madilyn@bystedts.com>
2022-12-07 19:09:40 +0100

    Add solution AOC 3 Part 1

--- AOC_3/Program.cs
+++ AOC_3/Program.cs
@@ -5,27 +5,49 @@
     */
     class Program
     {
+        private int sum;
         private void Init()
         {
            Console.WriteLine("*** INIT ***");
+           sum = 0;
         }
             
         // Part 1
-        private void TBD()
+        private void RucksackReorganization()
         {
             Init();
-            
+            Console.WriteLine("*** Rucksack Reorganisation ***");
             foreach (string line in File.ReadLines("puzzleinput.txt"))
             {
-                 Console.WriteLine("Line:" + line);
+                    
+                int lineLength = line.Length;
+                string rucksack1 = line.Substring(0, lineLength/2);
+                string rucksack2 = line.Substring(lineLength/2, lineLength/2); //Begin from half and and get from other half
+                var unique1 = new HashSet<char>(rucksack1); // Remove duplicated characters
+                var unique2 = new HashSet<char>(rucksack2); // Remove duplicated characters
+                int ascii = 0;
+
+                foreach(var c in unique1)
+                {   
+                    if(unique2.Contains(c)) { ascii = (int) c;}
+                }
+               
+                //Check lower case
+                if (ascii >= 97 && ascii <= 122)
+                    sum += (ascii - 96);
+                
+                // Check upper case
+                if (ascii >= 65 && ascii <= 90)
+                    sum += ((ascii - 65) + 27);
             }
+            Console.WriteLine("Sum: {0}", sum);
         }
 
 
         static void Main ()
         {
             Program p = new Program();
-            p.TBD();
+            p.RucksackReorganization();
         }
     }
 }
