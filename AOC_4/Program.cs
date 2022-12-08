308be2bd7d699b076ce9379123a0b8fedca0533f
Madilyn Bystedt <madilyn@bystedts.com>
2022-12-08 14:27:32 +0100

    Solution AOC day 4 part 1

---
+++ AOC_4/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace adventofcode
+{
+    /**
+    * Advent of code Day 4
+    */
+    class Program
+    {
+        private int sumPairs;
+        private void Init()
+        {
+            Console.WriteLine("*** Init ***");
+            sumPairs = 0;
+        }
+            
+        // Part 1
+        private void CampCleanup()
+        {
+            Init();
+            Console.WriteLine("*** Camp Cleanup ***");
+            foreach (string line in File.ReadLines("puzzleinput.txt"))
+            {
+                string[] sections = line.Split(',');
+                string[] range1 = sections[0].Split('-');
+                string[] range2 = sections[1].Split('-');
+
+                int leftMin = Int32.Parse(range1[0]);
+                int leftMax = Int32.Parse(range1[1]);
+                int rightMin = Int32.Parse(range2[0]);
+                int rightMax = Int32.Parse(range2[1]);
+
+                //Check if one is already fully contained by another team
+                if( (leftMin >= rightMin && leftMax <= rightMax) || (rightMin >= leftMin && rightMax <= leftMax) )
+                {
+                    ++sumPairs;
+                }
+                
+            }
+        }
+
+        static void Main ()
+        {
+            Program p = new Program();
+            //Part 1
+            p.CampCleanup();
+            Console.WriteLine("Sum pairs: {0}", p.sumPairs);
+            
+        }
+    }
+}
