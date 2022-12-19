cac8871f1ea3b2f4a0edd55d32c5364d297dd870
Madilyn Bystedt <madilyn@bystedts.com>
2022-12-19 16:13:50 +0100

    Add draft AOC day 7 part 1

---
+++ AOC_7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace adventofcode
+{
+    /**
+    * Advent of code Day 7
+    */
+    class Program
+    {
+        static readonly string[] input = File.ReadAllLines("input.txt");
+        private static void PartOne()
+        {
+            foreach (string line in input)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+
+        static void Main()
+        {
+            Console.WriteLine("Part 1");
+            PartOne();
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadLine();
+        }
+    }
+}
